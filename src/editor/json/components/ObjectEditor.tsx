ba3ce9881eef82819635aee4b72f683692ff88ff
Jianru Lin <jianru.lin@gmail.com>
2018-05-10 02:32:06 +0800

    better

--- src/editor/json/components/ObjectEditor.tsx
+++ src/editor/json/components/ObjectEditor.tsx
@@ -21,22 +21,21 @@ export class ObjectEditor extends React.Component<ObjectEditorP, ObjectEditorS> 
         const style: { [key: string]: React.CSSProperties } = {
             root: {
                 display: 'grid',
-                gridTemplateRows:
-                    node.manager.getLayout(node) === Layout.Vertical
-                        ? `repeat(${node.props.length}, auto)`
-                        : undefined,
-                gridTemplateColumns:
-                    node.manager.getLayout(node) === Layout.Horizontal
-                        ? `repeat(${node.props.length}, auto)`
-                        : undefined,
+                ...(node.manager.getLayout(node) === Layout.Vertical
+                    ? {
+                          gridTemplateRows: `repeat(${node.props.length}, auto)`,
+                          gridTemplateColumns: `auto auto`,
+                      }
+                    : null),
+                ...(node.manager.getLayout(node) === Layout.Horizontal
+                    ? {
+                          gridTemplateColumns: `repeat(${node.props.length * 2}, auto)`,
+                      }
+                    : null),
                 rowGap: '1em',
                 alignItems: 'baseline',
             },
-            pair: {
-                display: 'flex',
-                flexDirection: 'row',
-                alignItems: 'baseline',
-            },
+            pair: { display: 'flex', flexDirection: 'row', alignItems: 'baseline' },
             pair_name: {},
             pair_node: { display: 'flex', alignItems: 'baseline' },
             comma: { cursor: 'pointer' },
@@ -46,23 +45,26 @@ export class ObjectEditor extends React.Component<ObjectEditorP, ObjectEditorS> 
         if (node.props.length) {
             body = (
                 <div style={style.root}>
-                    {node.props.map((pair, i) => (
-                        <div style={style.pair}>
-                            <div style={style.pair_name}>{pair.name}:&nbsp;</div>
-                            <div style={style.pair_node}>
-                                {editorOfNode(pair.node)}
-                                {i < node.props.length - 1 ? (
-                                    <div
-                                        style={style.comma}
-                                        onClick={e => {
-                                            node.manager.toggleLayout(node);
-                                        }}>
-                                        ,&nbsp;
-                                    </div>
-                                ) : null}
-                            </div>
-                        </div>
-                    ))}
+                    {node.props.map(
+                        (pair, i) =>
+                            // <div style={style.pair}>
+                            [
+                                <div style={style.pair_name}>{pair.name}:&nbsp;</div>,
+                                <div style={style.pair_node}>
+                                    {editorOfNode(pair.node)}
+                                    {i < node.props.length - 1 ? (
+                                        <div
+                                            style={style.comma}
+                                            onClick={e => {
+                                                node.manager.toggleLayout(node);
+                                            }}>
+                                            ,&nbsp;
+                                        </div>
+                                    ) : null}
+                                </div>,
+                            ]
+                        // </div>
+                    )}
                 </div>
             );
         }
